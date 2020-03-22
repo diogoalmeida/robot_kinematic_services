a78516c51a4d7d70cdf3425563ca12f7e58b815b
Diogo Almeida <diogoa@kth.se>
2020-03-22 16:10:50 +0000

    Pass argc, argv to fixture

--- test/test_kinematic_services.cpp
+++ test/test_kinematic_services.cpp
@@ -1,4 +1,21 @@
 #include <gtest/gtest.h>
+#include <ros/ros.h>
+
+class TestKinematicServices : public ::testing::Test
+{
+ public:
+  void init(int argc, char** argv)
+  {
+    argc_ = argc;
+    argv_ = argv;
+  }
+
+ protected:
+  void SetUp() override { ros::init(argc_, argv_, "my_test_node"); }
+
+  int argc_;
+  char** argv_;
+};
 
 TEST(TestKinematicServices, TestParameterLoading) { EXPECT_TRUE(true); }
 
